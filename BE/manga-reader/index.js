27896b41d5f9b6da2569f735a74b36a0c46d0624
chocomint197 <153619592+chocomint197@users.noreply.github.com>
2024-05-28 20:31:57 +0700

    fix import

--- BE/manga-reader/index.js
+++ BE/manga-reader/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from 'dotenv';
 import connectDatabase from "./database/index.js";
 import RootRouter from "./routes/index.js";
+import cors from'cors';
+
 dotenv.config();
 
 const POST = process.env.PORT || 8080;
