1acb47fab549666ec504817db9d803c309af21bf
chocomint197 <153619592+chocomint197@users.noreply.github.com>
2024-05-30 16:51:25 +0700

    fix group router

--- BE/manga-reader/routes/ver1/group.js
+++ BE/manga-reader/routes/ver1/group.js
@@ -3,8 +3,8 @@ import middlewares from "../../middlewares/index.js";
 import groupController from "../../controllers/group/index.js";
 const GroupRouter = Router();
 GroupRouter.post('/create',  middlewares.verifyAccessToken, groupController.createGroup)
-GroupRouter.get('/list', middlewares.verifyAccessToken, groupController.getAllGroups );
-GroupRouter.get('/:id', middlewares.verifyAccessToken, groupController.groupProfile );
-GroupRouter.get('/:id/upload', middlewares.verifyAccessToken, groupController.groupUploads);
-GroupRouter.get('/:id/members', middlewares.verifyAccessToken,groupController.groupMembers )
+GroupRouter.get('/list', groupController.getAllGroups );
+GroupRouter.get('/:id', groupController.groupProfile );
+GroupRouter.get('/:id/upload', groupController.groupUploads);
+GroupRouter.get('/:id/members', groupController.groupMembers )
 export default GroupRouter;
